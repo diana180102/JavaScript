fd6f419e1964b3a65ddd171e9907fcd1d4bc4c2d
Diana Mayorga <diana101210@gmail.com>
2022-07-15 17:30:37 -0500

    resultado de Ejercicio Fibo

--- SumFibo.js
+++ SumFibo.js
@@ -1,5 +1,28 @@
 function sumFibs(num) {
-  return num;
+  
+   
+   let valorPreview = 0;
+   let valorA = 1;
+   let result = 0;
+   
+   
+   while (valorA<=num) {
+      
+      if (valorA % 2 !== 0) {
+         
+         result+= valorA;
+      }
+        
+        valorA = valorA + valorPreview;
+        valorPreview = valorA - valorPreview; 
+
+   }
+
+   return result;
+   
+   
+
+
 }
 
 sumFibs(4);
