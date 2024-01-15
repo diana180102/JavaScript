3e957fdfffa445aeb015e1451ba53afd52696378
Diana Mayorga Portatil <diana101210@gmail.com>
2024-01-15 14:44:51 -0500

    add dejaloCaer

--- dejaloCaer.js
+++ dejaloCaer.js
@@ -1,5 +1,29 @@
 function dropElements(arr, func) {
+  
+  let newArray = [];
+
+  // newArray = arr.filter(item => func(item));
+  
+
+    for (let i = 0; i < arr.length; i++) {
+       if(func(arr[0])){
+         break;
+        }else{
+           arr.shift();
+        }
+        
+      
+    }
+  
+
+  console.log(arr);
+
+  
+  
   return arr;
 }
 
-dropElements([1, 2, 3], function(n) {return n < 3; });
+// dropElements([1, 2, 3], function(n) {return n < 3; });
+dropElements([0, 1, 0, 1], function(n) {return n === 1;});
+
+// dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;});
